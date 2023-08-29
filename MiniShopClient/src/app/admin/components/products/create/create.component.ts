f5059aec9a3f4b4636c4f4b41823e263a845db60
berkgonencc <berkgonencc@gmail.com>
2023-08-29 13:38:51 +0300

    Fluent Validation with FluentFilter

--- MiniShopClient/src/app/admin/components/products/create/create.component.ts
+++ MiniShopClient/src/app/admin/components/products/create/create.component.ts
@@ -34,6 +34,23 @@ export class CreateComponent extends BaseComponent implements OnInit {
     create_product.price = parseFloat(price.value);
     create_product.stock = parseInt(stock.value);
 
+    if(!name.value ){
+      this.alertify.message('Please enter the product name!', {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight,
+      });
+      return;
+    }
+    if(parseInt(stock.value) < 0){
+      this.alertify.message('Please enter the stock number!', {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight,
+      });
+      return;
+    }
+
     this.productService.create(
       create_product,
       () => {
