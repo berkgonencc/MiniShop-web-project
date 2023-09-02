5a5c0503c1c9c7d28ee8bb6db39f60958477e7bc
berkgonencc <berkgonencc@gmail.com>
2023-09-02 19:40:05 +0300

    delete module added

--- MiniShopClient/src/app/directives/admin/delete.directive.ts
+++ MiniShopClient/src/app/directives/admin/delete.directive.ts
@@ -7,10 +7,17 @@ import {
   Output,
   Renderer2,
 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
+import {
+  DeleteDialogComponent,
+  DeleteState,
+} from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
+
 declare var $: any;
+
 @Directive({
   selector: '[appDelete]',
 })
@@ -19,7 +26,8 @@ export class DeleteDirective {
     private element: ElementRef,
     private _renderer: Renderer2,
     private productService: ProductService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    public dialog: MatDialog
   ) {
     const img = _renderer.createElement('img');
     img.setAttribute('src', '../../../../../assets/delete.png');
@@ -34,11 +42,34 @@ export class DeleteDirective {
 
   @HostListener('click')
   async onclick() {
-    this.spinner.show(SpinnerType.BallAtom);
-    const td: HTMLTableCellElement = this.element.nativeElement;
-    await this.productService.delete(this.id);
-    $(td.parentElement).fadeOut(2000, () => {
-      this.callback.emit();
+    this.openDialog(async () => {
+      this.spinner.show(SpinnerType.BallAtom);
+      const td: HTMLTableCellElement = this.element.nativeElement;
+      await this.productService.delete(this.id);
+      $(td.parentElement).animate(
+        {
+          opacity: 0,
+          left: '+=50',
+          height: 'toogle',
+        },
+        700,
+        () => {
+          this.callback.emit();
+        }
+      );
+    });
+  }
+
+  openDialog(afterClosed: any): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: DeleteState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result == DeleteState.Yes) {
+        afterClosed();
+      }
     });
   }
 }
