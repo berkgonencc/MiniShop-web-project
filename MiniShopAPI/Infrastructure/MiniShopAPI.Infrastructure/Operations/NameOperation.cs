a5bea3aedf7a4ce0ecfbda3c02e798691e1571d6
berkgonencc <berkgonencc@gmail.com>
2023-09-07 23:41:30 +0300

    rename operation added

---
+++ MiniShopAPI/Infrastructure/MiniShopAPI.Infrastructure/Operations/NameOperation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace MiniShopAPI.Infrastructure.Operations
+{
+    public static class NameOperation
+    {
+        public static string CharacterRegulatory(string name)
+            => name.Replace("\"", "")
+                    .Replace("!", "")
+                    .Replace("'", "")
+                    .Replace("^", "")
+                    .Replace("+", "")
+                    .Replace("%", "")
+                    .Replace("&", "")
+                    .Replace("/", "")
+                    .Replace("(", "")
+                    .Replace(")", "")
+                    .Replace("=", "")
+                    .Replace("?", "")
+                    .Replace("_", "")
+                    .Replace("", "")
+                    .Replace("@", "")
+                    .Replace("£", "")
+                    .Replace("~", "")
+                    .Replace("¨", "")
+                    .Replace(",", "")
+                    .Replace(";", "")
+                    .Replace(":", "")
+                    .Replace(".", "-")
+                    .Replace("Ö", "o")
+                    .Replace("ö", "o")
+                    .Replace("Ü", "u")
+                    .Replace("ü", "u")
+                    .Replace("ı", "i")
+                    .Replace("İ", "i")
+                    .Replace("ğ", "g")
+                    .Replace("Ğ", "g")
+                    .Replace("æ", "")
+                    .Replace("ş", "s")
+                    .Replace("ç", "c")
+                    .Replace("Ş", "s")
+                    .Replace("Ç", "c")
+                    .Replace(">", "")
+                    .Replace("<", "")
+                    .Replace("|", "");
+    }
+}
+
--- MiniShopAPI/Infrastructure/MiniShopAPI.Infrastructure/Services/FileService.cs
+++ MiniShopAPI/Infrastructure/MiniShopAPI.Infrastructure/Services/FileService.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using MiniShopAPI.Application.Services;
+using MiniShopAPI.Infrastructure.Operations;
 
 namespace MiniShopAPI.Infrastructure.Services
 {
@@ -18,8 +19,8 @@ namespace MiniShopAPI.Infrastructure.Services
         {
             try
             {
-                await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
-                await fileStream.CopyToAsync(fileStream);
+                using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
                 return true;
             }
@@ -31,9 +32,53 @@ namespace MiniShopAPI.Infrastructure.Services
 
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+        async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
         {
-            throw new NotImplementedException();
+            string newFileName = await Task.Run<string>(async () =>
+            {
+                string extension = Path.GetExtension(fileName);
+                string newFileName = string.Empty;
+
+                if (first)
+                {
+                    string oldName = Path.GetFileNameWithoutExtension(fileName);
+                    newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
+                }
+                else
+                {
+                    newFileName = fileName;
+                    int indexNo1 = newFileName.IndexOf("-");
+                    if (indexNo1 == -1)
+                    {
+                        newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extension}";
+                    }
+                    else
+                    {
+                        int indexNo2 = newFileName.IndexOf(".");
+                        string fileNo = newFileName.Substring(indexNo1, indexNo2 - indexNo1 - 1);
+                        int _fileNo = int.Parse(fileNo);
+                        _fileNo++;
+
+                        newFileName = newFileName.Remove(indexNo1, indexNo2 - indexNo1 - 1)
+                                                 .Insert(indexNo1, _fileNo.ToString());
+
+                    }
+                }
+
+                if (File.Exists($"{path}\\{newFileName}"))
+                {
+                    return await FileRenameAsync(path, newFileName, false);
+                }
+                else
+                {
+                    return newFileName;
+                }
+
+
+
+            });
+            return newFileName;
+
         }
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
@@ -49,7 +94,7 @@ namespace MiniShopAPI.Infrastructure.Services
 
             foreach (IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
+                string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
 
                 bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
                 datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
